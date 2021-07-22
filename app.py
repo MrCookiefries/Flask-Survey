6c198a8c4887880650136060abf946a64fce8711
MrCookiefries <cookiefries12@gmail.com>
2021-07-21 19:44:38 -0500

    finish project

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import re
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from surveys import satisfaction_survey
@@ -9,19 +8,23 @@ app.config["SECRET_KEY"] = "TacoCatIsCoolMan"
 
 debug = DebugToolbarExtension(app)
 
-# stores responses from user input on survey
-responses = []
-
 @app.route("/")
 def landing_page():
     """shows the home page"""
     survey = satisfaction_survey
     return render_template("home.html", survey=survey)
 
+@app.route("/start", methods=["POST"])
+def start_request():
+    """starts the survey"""
+    # stores responses from user input on survey
+    session["responses"] = []
+    return redirect("/questions/0")
+
 @app.route("/questions/<int:question_num>")
 def question_page(question_num):
     """shows the page with the current survey question"""
-    num_length = len(responses)
+    num_length = len(session["responses"])
     if question_num != num_length:
         flash("Trying to access an invalid question!", "warning")
         return redirect(f"/questions/{num_length}")
@@ -31,8 +34,11 @@ def question_page(question_num):
 @app.route("/answer", methods=["POST"])
 def answer_page():
     """gets the answer from the question page & redirects to next question"""
-    responses.append(request.form.get("answer"))
-    num = len(responses)
+    resps = session["responses"]
+    answer = request.form.get("answer")
+    resps.append(answer)
+    session["responses"] = resps
+    num = len(session["responses"])
     if num < len(satisfaction_survey.questions):
         return redirect(f"/questions/{num}")
     return redirect("/thanks")
